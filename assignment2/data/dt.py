b9983b7e50e85c9dfdacd46b7d1710327ef2e02c
hyun subin <sunny5875@hanyang.ac.kr>
2023-04-28 18:13:50 +0900

    add assignment2

--- assignment2/data/dt.py
+++ assignment2/data/dt.py
@@ -1,75 +1,84 @@
 
 import sys
 import numpy as np
-
+import random
 
 
 class Feature:
-    def __init__(self, feature):
+    def __init__(self, feature, i):
         self.featureName = feature
         self.featureValueList = []
-        self.isValid = True
+        self.tag = i
     
-    def getFeatureValueList(self) :
-        return self.featureValueList
-    
-    def setFeatureValueList(self, value) :
-        self.featureValueList.append(value)
     
     def __str__(self):
         return f"featureName: ({self.featureName})\t featureValueList: ({self.featureValueList})"
+    
+    def __iter__(self):
+        return iter(self.featureValueList)
 
 class Node:
-    def __init__(self, feat):
-        self.feature = feat
-        self.children = []
-        self.class_label = None
+    def __init__(self):
+        self.feature = None #feature 종류, nonleaf이면 값이 있고 아니면 None임
+        self.children = [] #child 노드
+        self.class_label = None #leaf node일 때 class_label
+        self.feature_value = None # 부모노드 feature에 의해 나눠질 때의 해당 feature value
 
     def append(self, newNode):
         self.next.append(newNode)
 
-    def setFeature(self, feat):
-        self.feature = feat
+    
+    def __repr__(self):
+        return f"Node(feature: {self.feature}, class_label: {self.class_label}),childern: {self.children}), feature_value: {self.feature_value})"
 
-    def setClassLabel(self, result):
-        self.class_label = result
-
-    def getFeature(self):
-        return self.feature
-
-    def getNext(self, index):
-        return self.children[index]
-
-    def getClassLabel(self):
-        return self.class_label
+    def dfs(self):
+        print(self)
+        for child in self.children :
+            child.dfs()
     
     
     # evaluate entropy of dataset
     def calculate_before_entropy(self, class_labels, data_set):
         entropy = 0
         data_labels = np.array([])
-        for item in class_labels:
-            data_labels.append(item[-1])
+        for item in data_set:
+            data_labels = np.append(data_labels, item[-1])
+
 
         for label in class_labels:
             p_i = np.sum(data_labels==label)/data_labels.size
             if p_i==0:
                 continue
             entropy += p_i*np.log2(p_i)
-
         return -entropy
 
     # evaluate entropy of splited dataset
-    def calculate_after_entropy(self, class_labels, data_set, attr_idx):
+    def calculate_after_entropy(self, class_labels, data_set, feature):
         entropy = 0
-        attr_values = np.unique(data_set.T[attr_idx])
+        attr_values = feature.featureValueList
 
         for attr in attr_values:
-            data_subset = data_set[data_set.T[attr_idx]==attr]
+            data_subset = data_set[data_set.T[feature.tag]==attr]
             p_i = data_subset.shape[0]/data_set.shape[0]
+            
             entropy += p_i*self.calculate_before_entropy(class_labels, data_subset)
 
+        print("using feature ",feature.featureName," after entropy: ", entropy)
         return entropy
+    
+    def calculate_splitInfo(self, data_set, feature):
+        entropy = 0
+        attr_values = feature.featureValueList
+
+        for attr in attr_values:
+            data_subset = data_set[data_set.T[feature.tag]==attr]
+            p_i = data_subset.shape[0]/data_set.shape[0]
+            if p_i==0:
+                continue
+            entropy += p_i*np.log2(p_i)
+
+        print("using feature ",feature.featureName," splitInfo: ", -entropy)
+        return -entropy
     
     def majority_voting(self, class_labels, data_set):
         major_label = None
@@ -80,86 +89,109 @@ class Node:
             if cnt > max_cnt:
                 max_cnt = cnt
                 major_label = label
+        # return major_label, max_cnt/data_labels.size
+        return major_label
 
-        return major_label, max_cnt/data_labels.size
-
-    def fit(self, data_set, remainfeatureList, class_labels):
-        before_entropy = self.calculate_before_entropy(class_labels, data_set)
-        if before_entropy == 0:
-            self.class_label = data_set[0][-1]
+    def fit(self, data_set, remainfeatureList, class_label):
+        #split할지 말지 결정
+        print("===== start", self, "=====")
+        before_entropy = self.calculate_before_entropy(class_label, data_set)
+        print("before entropy: ", before_entropy)
+        
+       
+        if before_entropy == 0 or remainfeatureList.size == 0 :
+            self.class_label = self.majority_voting(class_label, data_set)
+            print("done")
             return
 
-        self.class_label, ratio = self.majority_voting(class_labels, data_set)
-        if len(self.mask) == attributes.size - 1 or ratio > self.majority_threshold:
-            return
 
-        test_attr_idx = None
-        max_info_gain = 0
-
-        for attr_idx in range(attributes.size - 1):
-            if attr_idx in self.mask:
-                continue
+        max_feature = remainfeatureList[0]
+        max_gain_ratio = sys.float_info.min
+        
+        for feature in remainfeatureList:
+                info_gain = before_entropy - self.calculate_after_entropy(class_label, data_set, feature)
+                split_info = self.calculate_splitInfo(data_set, feature)
+                gain_ratio = info_gain / split_info
+                if gain_ratio > max_gain_ratio:
+                    max_gain_ratio = gain_ratio
+                    max_feature = feature
+        
+        print("select the feature!: ", max_feature)
+        self.feature = max_feature
+        
+        for feature_value in max_feature.featureValueList:
+            data_subset = data_set[data_set.T[max_feature.tag]==feature_value]
             
-            info_gain = self.calculate_before_entropy(class_labels, data_set) - self.calculate_after_entropy(class_labels, data_set, attr_idx)
-            if info_gain > max_info_gain:
-                max_info_gain = info_gain
-                test_attr_idx = attr_idx
-        
-
-        self.feature = remainfeatureList[test_attr_idx]
-        attr_values = np.unique(data_set.T[test_attr_idx])
-        
-        for attr in attr_values:
-            data_subset = data_set[data_set.T[test_attr_idx]==attr]
             subset_ratio = data_subset.shape[0]/data_set.shape[0]
-            if subset_ratio < self.pruning_threshold:
-                continue
-            new_leaf = Node(new_mask)
 
-            new_leaf.fit(class_labels, data_subset)
-            self.children[attr] = new_leaf
+            new_remain_feature_list = np.delete(remainfeatureList, np.where(remainfeatureList == max_feature))
+            new_leaf = Node()
+            new_leaf.feature_value = feature_value
+
+            if data_subset.size != 0:
+                new_leaf.fit( data_subset, new_remain_feature_list ,class_label)
+            else:
+                new_leaf.class_label = random.choice(class_label.featureValueList)
+            self.children.append(new_leaf)
             
 
 
-    def majorityVoting(dataSet):
-        return 'majority'
+    def predict(self, data):
+        if self.feature == None:
+            return self.class_label
         
-    def predict(model) -> str:
-        return 'unknown'
-
- 
-
+        for i, item in enumerate(self.feature.featureValueList):
+            if item == data[self.feature.tag]:
+                return self.children[i].predict(data)
 
         
     
 def read_train_file(file_name):
     f = open("./" + file_name,'r')
 
-    transaction_list = []
 
     read_feature_list = f.readline().split()
-    feature_list = []
-
-    for feature in read_feature_list:
-        feature_list.append(Feature(feature))
+    feature_list = np.array([])
+    for i, feature in enumerate(read_feature_list):
+        feature_list = np.append(feature_list, np.array([Feature(feature, i )]))
     
     input_list = f.readlines()
+    transaction_list = np.empty((0,feature_list.size))
+
 
     for input in input_list:
         item_set = np.array(input.strip().split('\t'))
-        transaction_list.append(item_set)
+        transaction_list = np.append(transaction_list,  np.array([item_set]), axis = 0)
     
         for i, item in enumerate(item_set):
-            if item not in feature_list[i].getFeatureValueList():
-                feature_list[i].setFeatureValueList(item)
+            if item not in feature_list[i].featureValueList:
+                feature_list[i].featureValueList.append(item)
     
     f.close()
+    class_label = feature_list[-1]
+    feature_list = feature_list[:-1]
     
-    return feature_list, transaction_list
+
+    return feature_list, class_label, transaction_list
 
 
+def create_output_file(output_file_name, test_file_name, feature_list, class_label):
+    output_file = open(output_file_name, 'w')
+    _, _, test_data = read_train_file(test_file_name)
+    result = ""
+    for item in feature_list:
+        result += item.featureName+"\t"
+    
+    output_file.write(result + class_label.featureName + "\n")
 
+    for data in test_data:
+        result = ""
+        for item in data:
+            result += item+"\t"
+        result += model.predict(data) +"\n"
+        output_file.write(result)
 
+    output_file.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -170,19 +202,18 @@ if __name__ == "__main__":
     test_file_name = sys.argv[2]
     output_file_name = sys.argv[3]
 
-    feature_list, transaction_list = read_train_file(train_file_name)
+    feature_list, class_label, transaction_list = read_train_file(train_file_name)
 
     print("===== initial feature list =====")
     for item in feature_list:
         print(item)
+    print(class_label)
     print("===== initial transaction list =====")
     print(transaction_list)
 
-    model = Node(feature_list)
-    model.fit(model, transaction_list, feature_list)
+    model = Node()
+    model.fit( transaction_list, feature_list, class_label)
+    # model.dfs()
 
-    predict(model)
-
-    # output_file = open(output_file_name, 'w')
-    # output_file.write()
-    # output_file.close()
+    create_output_file(output_file_name, test_file_name, feature_list, class_label)
+    
--- assignment2/data/t2.py
+++ assignment2/data/t2.py
@@ -1,71 +1,47 @@
 import sys
+import time
 import numpy as np
 
-class Feature:
-    def __init__(self, feature):
-        self.featureName = feature
-        self.featureValueList = []
-        self.isValid = True
-    
-    def getFeatureValueList(self) :
-        return self.featureValueList
-    
-    def setFeatureValueList(self, value) :
-        self.featureValueList.append(value)
-    
-    def __str__(self):
-        return f"featureName: ({self.featureName})\t featureValueList: ({self.featureValueList})"
-    
-    def getFeatureName(self):
-        return self.featureName
-
-
 # Decision Tree Class
-class Node:
-    def __init__(self, feature):
-        self.children = {}
-        self.feature = feature
-        # self.attr_idx = None
-        # self.mask = set()
-        # self.mask.update(mask)
+class DT:
+    # Constructor
+    # mask is set of already used attribute index
+    def __init__(self, mask):
+        self.child = {}
+        self.attr_idx = None
+        self.mask = set()
+        self.mask.update(mask)
         self.class_label = None
-        # self.majority_threshold = 0.8
-        # self.pruning_threshold = 0.05
-    
-    def __repr__(self):
-        return f"Node(feature: {self.feature}, class_label: {self.class_label})"
+        self.majority_threshold = 0.8
+        self.pruning_threshold = 0.05
 
-    def dfs(self):
-        print(self)
-        for child in self.children:
-            child.dfs()
-
-    def calculate_before_entropy(self, class_labels, data_set):
+    # evaluate entropy of dataset
+    def entropy_of(self, class_labels, data_set):
         entropy = 0
         data_labels = data_set.T[-1]
-        print(data_labels)
         for label in class_labels:
             p_i = np.sum(data_labels==label)/data_labels.size
-            print(p_i)
             if p_i==0:
                 continue
             entropy += p_i*np.log2(p_i)
 
         return -entropy
 
-    def calculate_after_entropy(self, class_labels, data_set, attr_idx):
+    # evaluate entropy of splited dataset
+    def entropy_with_attr_of(self, class_labels, data_set, attr_idx):
         entropy = 0
         attr_values = np.unique(data_set.T[attr_idx])
-        print(attr_values)
 
         for attr in attr_values:
             data_subset = data_set[data_set.T[attr_idx]==attr]
             p_i = data_subset.shape[0]/data_set.shape[0]
-            entropy += p_i*self.calculate_before_entropy(class_labels, data_subset)
+            entropy += p_i*self.entropy_of(class_labels, data_subset)
 
         return entropy
     
-    def majority_voting(self, class_labels, data_set):
+    # majority voting
+    # return major class label and its ratio
+    def major_label_of(self, class_labels, data_set):
         major_label = None
         max_cnt = 0
         data_labels = data_set.T[-1]
@@ -77,16 +53,16 @@ class Node:
 
         return major_label, max_cnt/data_labels.size
 
-    def fit(self, feature_list, data_set):
-        print("start fit")
-        if self.calculate_before_entropy(feature_list, data_set) == 0:
-            print("done")
+    # construct Decision Tree recursively
+    # pruning with majority_threshold and pruning_threshold
+    def construct(self, attributes, class_labels, data_set):
+        if self.entropy_of(class_labels, data_set) == 0:
             self.class_label = data_set[0][-1]
             return
-        print("not done")
-        self.class_label, ratio = self.majority_voting(feature_list, data_set)
-        # if len(self.mask) == attributes.size - 1 or ratio > self.majority_threshold:
-        #     return
+        
+        self.class_label, ratio = self.major_label_of(class_labels, data_set)
+        if len(self.mask) == attributes.size - 1 or ratio > self.majority_threshold:
+            return
 
         test_attr_idx = None
         max_info_gain = 0
@@ -95,87 +71,93 @@ class Node:
             if attr_idx in self.mask:
                 continue
             
-            info_gain = self.calculate_before_entropy(feature_list, data_set) - self.calculate_after_entropy(feature_list, data_set, attr_idx)
+            info_gain = self.entropy_of(class_labels, data_set) - self.entropy_with_attr_of(class_labels, data_set, attr_idx)
             if info_gain > max_info_gain:
                 max_info_gain = info_gain
                 test_attr_idx = attr_idx
         
+        new_mask = set()
+        new_mask.update(self.mask)
+        new_mask.add(test_attr_idx)
 
-        attr_idx = test_attr_idx
+        self.attr_idx = test_attr_idx
         attr_values = np.unique(data_set.T[test_attr_idx])
         
         for attr in attr_values:
             data_subset = data_set[data_set.T[test_attr_idx]==attr]
             subset_ratio = data_subset.shape[0]/data_set.shape[0]
-            # if subset_ratio < self.pruning_threshold:
-            #     continue
-            new_leaf = Node(new_mask)
-            new_feature_list = feature_list
-            new_feature_list[attr_idx].isValid = False
+            if subset_ratio < self.pruning_threshold:
+                continue
+            new_leaf = DT(new_mask)
 
-            new_leaf.fit(feature_list, data_subset)
-            self.children[attr] = new_leaf
+            new_leaf.construct(attributes, class_labels, data_subset)
+            self.child[attr] = new_leaf
 
-    def predict(self, data):
-        if self.feature == None:
+    # predict unknown class label of given data
+    def classify(self, data):
+        if self.attr_idx == None:
             return self.class_label
         
         attr = data[self.attr_idx]
 
-        if not (attr in self.children):
+        if not (attr in self.child):
             return self.class_label
 
-        return self.children[attr].predict(data)
+        return self.child[attr].classify(data)
 
-
+# read input file
+# Since DT algorithm in this source code is using vector operation with numpy,
+# Dataset is parsed to numpy array
+# To eliminate randomness, class labels are sorted
 def input_training_set(filename):
     file = open(filename)
     
-    read_feature_list = file.readline().split()
-    feature_list = []
-
-    for feature in read_feature_list:
-        feature_list.append(Feature(feature))
-        
+    attributes = np.array(file.readline().strip().split('\t'))
+    class_labels = []
     training_set = []
 
     while True:
         line = file.readline()
         if not line:
             break
-        item_set = np.array(line.strip().split('\t'))
-        training_set.append(item_set)
-        for i, item in enumerate(item_set):
-            if item not in feature_list[i].getFeatureValueList():
-                feature_list[i].setFeatureValueList(item)
+        data = np.array(line.strip().split('\t'))
+        class_labels.append(data[-1])
+        training_set.append(data)
     
     training_set = np.array(training_set)
+    class_labels = np.unique(class_labels)
+    class_labels.sort()
     file.close()
     
-    return feature_list, training_set
+    return attributes, class_labels, training_set
 
-def build_decision_tree(class_labels, training_set):
-    decision_tree = Node(None)
-    decision_tree.fit(class_labels, training_set)
+# master Decision Tree Building Process
+# using DT class, it returns a Decision Tree instance
+def build_decision_tree(attributes, class_labels, training_set):
+    decision_tree = DT(set())
+
+    decision_tree.construct(attributes, class_labels, training_set)
     
     return decision_tree
 
+# formatting function for numpy array with string
 def array_to_str(arr):
     result = ''
     for s in arr:
-        result += s.getFeatureName()
+        result += s
         result += '\t'
     result = result[:-1]
     result += '\n'
 
     return result
 
-def test_and_output(feature_list, decision_tree, test_filename, output_filename): 
+# read test file and write test result to output file
+def test_and_output(attributes, decision_tree, test_filename, output_filename): 
     test_file = open(test_filename)
     output_file = open(output_filename, mode='w')
 
     test_file.readline()
-    output_file.write(array_to_str(feature_list))
+    output_file.write(array_to_str(attributes))
 
     while True:
         line = test_file.readline()
@@ -183,29 +165,30 @@ def test_and_output(feature_list, decision_tree, test_filename, output_filename)
             break
         data = np.array(line.strip().split('\t'))
 
-        class_label = decision_tree.predict(data)
+        class_label = decision_tree.classify(data)
         data = np.append(data, class_label)
         output_file.write(array_to_str(data))
     
     test_file.close()
     output_file.close()
 
+
+
 def main(argv):
     if len(argv)<4:
         print('PLEASE, give 3 arguments (training filename, test filename, output filename)')
         return
-    feature_list, transaction_list = input_training_set(argv[1])
-
-    print("===== initial feature list =====")
-    for item in feature_list:
-        print(item)
-    print("===== initial transaction list =====")
-    print(transaction_list)
+    attributes, class_labels, training_set = input_training_set(argv[1])
     
-    # sys.setrecursionlimit(max(attributes.size*10, 10000))
-    decision_tree = build_decision_tree(feature_list, transaction_list)
-    decision_tree.dfs()
-    # test_and_output(class_labels, decision_tree, argv[2], argv[3])
+    sys.setrecursionlimit(max(attributes.size*10, 10000))
+    train_start = time.time()
+    decision_tree = build_decision_tree(attributes, class_labels, training_set)
+    train_end = time.time()
+    print('Building Time : %f sec' % (train_end-train_start))
+    test_start = time.time()
+    test_and_output(attributes, decision_tree, argv[2], argv[3])
+    test_end = time.time()
+    print('Testing Time : %f sec' % (test_end-test_start))
 
 if __name__ == '__main__':
     main(sys.argv)
